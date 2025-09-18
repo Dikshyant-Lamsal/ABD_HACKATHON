b4a4de9d50710cb79510a88cdffb0883ad610e09
Dikshyant-Lamsal <140510854+Dikshyant-Lamsal@users.noreply.github.com>
2025-09-18 20:20:35 +0530

    fix: update root route to render Landing component instead of Login

--- eduGenie/frontend/src/App.jsx
+++ eduGenie/frontend/src/App.jsx
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/signup"; // make sure you have this
 import Dashboard from "./pages/Dashboard";
+import Landing from "./components/landing";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
